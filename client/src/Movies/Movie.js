4e7996d136fdda03b4d63ba93b7779a8544d9591
Donald Whitely <dswhitely1@gmail.com>
2019-06-10 11:29:44 -0400

    DRY Stretch Complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class Movie extends Component {
-  constructor(props) {
-    super(props);
+  constructor( props ) {
+    super( props );
     this.state = {
       movie: null
     };
@@ -12,18 +13,18 @@ export default class Movie extends Component {
   componentDidMount() {
     // change this line to grab the id passed on the URL
     const id = this.props.match.params.id;
-    this.fetchMovie(id);
+    this.fetchMovie( id );
   }
 
   fetchMovie = id => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
-        this.setState(() => ({ movie: response.data }));
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      .get( `http://localhost:5000/api/movies/${id}` )
+      .then( response => {
+        this.setState( () => ({movie: response.data}) );
+      } )
+      .catch( error => {
+        console.error( error );
+      } );
   };
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
@@ -41,28 +42,10 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
-    const { title, director, metascore, stars } = this.state.movie;
     return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+      <div className='save-wrapper'>
+        <MovieCard movie={this.state.movie} />
         <div className="save-button">Save</div>
-      </div>
-    );
+      </div>);
   }
 }
