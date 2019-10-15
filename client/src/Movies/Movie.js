bec96369ab02e249b1284ec2f6a91636abc6bdbe
Donald Whitely <dswhitely1@gmail.com>
2019-10-15 11:45:35 -0400

    Refactored To Function base Component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,54 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MovieCard from './MovieCard';
 
-export default class Movie extends Component {
-  constructor( props ) {
-    super( props );
-    this.state = {
-      movie: null
-    };
-  }
+function Movie ({route: {match: {params}}, addToSavedList}) {
+  const [movie, setMovie] = useState(null);
 
-  componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = this.props.route.match.params.id;
-    this.fetchMovie( id );
-  }
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/movies/${params.id}`)
+      .then(res => setMovie(res.data))
+      .catch(err => console.log(err.response));
+  }, [params.id]);
+  // componentDidMount();
+  // {
+  //   // change this line to grab the id passed on the URL
+  //   const id = this.props.route.match.params.id;
+  //   this.fetchMovie(id);
+  // }
 
-  fetchMovie = id => {
-    axios
-      .get( `http://localhost:5000/api/movies/${id}` )
-      .then( response => {
-        this.setState( () => ({movie: response.data}) );
-      } )
-      .catch( error => {
-        console.error( error );
-      } );
-  };
+  // fetchMovie = id => {
+  //   axios
+  //     .get(`http://localhost:5000/api/movies/${id}`)
+  //     .then(response => {
+  //       this.setState(() => ({ movie: response.data }));
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // };
 
   // Uncomment this code when you're ready for the stretch problems
-  componentWillReceiveProps(newProps){
-    console.log(newProps);
-    if(this.props.route.match.params.id !== newProps.route.match.params.id){
-      this.fetchMovie(newProps.route.match.params.id);
-    }
-  }
+  // componentWillReceiveProps(newProps);
+  // {
+  //   console.log(newProps);
+  //   if (this.props.route.match.params.id !== newProps.route.match.params.id) {
+  //     this.fetchMovie(newProps.route.match.params.id);
+  //   }
+  // }
 
+  // saveMovie = () => {
+  //   const addToSavedList = this.props.addToSavedList;
+  //   addToSavedList(this.state.movie);
+  // };
 
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie)
+  if (!movie) {
+    return <div>Loading movie information...</div>;
   }
+  return (
+    <div className='save-wrapper'>
+      <MovieCard movie={movie} />
+      <div
+        className='save-button'
+        onClick={() => addToSavedList(movie)}
+      >Save
+      </div>
+    </div>);
 
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
-    return (
-      <div className='save-wrapper'>
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>Save</div>
-      </div>);
-  }
 }
+
+export default Movie;
